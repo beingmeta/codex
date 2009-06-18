5d6e9d9f1ec0237f92fa98309f8961f544de7344
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-06-18 22:14:52 +0000

    Fixed false.true typo

--- sbooks.js
+++ sbooks.js
@@ -418,7 +418,7 @@ function sbook_toc_builder(child,tocstate)
     i=0; while (i<tagstack.length) {
       var ctags=tagstack[i++];
       var j=0; while (j<ctags.length)  {
-	sbookAddTag(child,ctags[j++],false.true,true,tocstate.knowlet);}}
+	sbookAddTag(child,ctags[j++],false,true,true,tocstate.knowlet);}}
     if (level>0) tocstate.tagstack.push(knowdes);
     _total_tagged_count++;}
   else if (level) tocstate.tagstack.push([]);
