98dede0a62771eccb432bdcab626923609fcb398
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-10-02 20:15:19 +0000

    Small fixes, especially to docs

--- helptext.js
+++ helptext.js
@@ -2,10 +2,11 @@ var sbook_helptext=
   "<div class='helphelp left' style='cursor: pointer;' title='click to hide' onclick='sbookHUDMode(false); return false;'><img src='http://static.beingmeta.com/graphics/redx16x16.png' alt='x' style='color: red;'/>Type 'h' to toggle this help</div>\
 <div class='helphelp right'>\
 Hide this help on startup<input type='CHECKBOX' name='SBOOKHIDEHELP' class='helphider' VALUE='yes'></div>\
-<h1>Welcome to sBooks! <div class='motto'>&ldquo;possibly the best thing since sliced paper&rdquo;</div></h1>\
-<p>The sBooks <strong>ambient UI</strong> uses an overlay or HUD (heads up \
-display) to provide <em>context and interaction</em>.  <strong>Hold the \
-shift key</strong> to see the HUD.  The sBooks HUD is divided into four \
+<div class='sbookh1'>Welcome to sBooks! <div class='motto'>&ldquo;possibly the best thing since sliced paper&rdquo;</div></div>\
+<p>The sBooks translucent <strong>HUD</strong> (heads up \
+display) provides <em>context and interaction</em> for the book's content.  \
+The sBooks <strong>display HUD</strong> is available anywhere but is normally invisible or light gray.  Holding down the<strong>shift key</strong> \
+makes the displays more visible.  The display HUD is divided into four \
 elements:</p><ul> <li>the <span class='hudref' hudref='SBOOKTOC'>TOC \
 HUD</span> on the top <strong>for navigation</strong></li> \
 <li>the <span class='hudref' hudref='SBOOKECHOES'>Meta HUD</span>, on the \
@@ -13,21 +14,21 @@ left, <strong>to browse comments</strong> </li> <li>the <span class='hudref' hud
 HUD</span> on the bottom <strong>to search tags</strong>, and</li> <li>individual <strong>note \
 icons</strong> in the <span class='hudref' hudref='SBOOKRIGHTMARGIN'>right \
 hand margin</span>.</li></ul>\
-<p>These HUDs are <strong>automatically updated</strong> as you read the \
+<p><strong>Preview icons</strong> (<img src='http://static.beingmeta.com/graphics/EyeIcon19x15.png'/>) \
+appear throughout the HUD: moving the \
+mouse over these icons <em>temporarily previews</em> the \
+content, scrolling to the referenced point in the document and dimming the \
+HUD.  Clicking goes to the displayed content; \
+moving the mouse away restores the document location and returns to \
+the HUD.</p>\
+<p>Display HUDs are <strong>automatically updated</strong> as you read the \
 document: moving the <strong>mouse over</strong> a HUD makes it more visible \
-and <strong>clicking</strong> expands the displayed information and \
-makes it persistent <strong>until you click again</strong>.<br/>\
+and <strong>clicking</strong> expands the displayed information, \
+making it persistent <strong>until you click again</strong>.<br/>\
 You can <strong>make (and share) notes</strong> in your sBook by clicking on \
 a passage or heading.  Providing you're logged into sBooks (and Facebook), \
 you can then a brief comment together with tags, longer details, excerpts, \
 or cross references.</p>\
-<p><strong>Preview icons</strong> (<img src='http://static.beingmeta.com/graphics/EyeIcon19x15.png'/>) \
-appear throughout the HUD: moving the \
-mouse over these icons <strong>temporarily previews</strong> the \
-content, scrolling to the referenced segment of the document and dimming the \
-HUD.  Clicking at this point goes to the displayed content while \
-moving the mouse away returns to the preceding location and returns to \
-the HUD.</p>\
 <p>The <span class='hudref' hudref='SBOOKTOC'>TOC \
 HUD</span> has <em>collapsed</em> and <em>expanded</em> forms and clicking on the \
 HUD or the <span class='hudref' hudref='SBOOKNAVBUTTON'>NAV \
@@ -37,7 +38,7 @@ indicating the <em>surrounding sections</em>.  The lengths of the spans indicate
 the relative length of the sections and the current section is yellow.  <strong>Moving \
 the mouse</strong> over a span <em>previews</em> the head of the corresponding section, \
 analogous to flipping through the pages of a physical book.  <strong>Clicking</strong> jumps to \
-the previewd section.  In the <strong>expanded view</strong>, all of the sections above the \
+the previewed section.  In the <strong>expanded view</strong>, all of the sections above the \
 current section are displayed from top to bottom and each works just as the collapsed view.</p>\
 <p>In the <span class='hudref' hudref='SBOOKSEARCH'>Index \
 HUD</span>, you can search the book using <strong>tags</strong> assigned to content \
@@ -47,7 +48,7 @@ subscribed metadocs.  The Index HUD is especially designed to help you search fo
 spaces &mdash; are <strong>separated by semicolons</strong>.  Above the text field is a <em>cloud of possible \
 tags</em> and a <em>result count</em> based on the complete tags you've typed so far and the tag you are currently typing. \
 The <strong>cloud of tags</strong> is ordered by <em>frequency</em> among the <em>current search results</em>.  \
-Clicking a tag adds that tag to the search; clicking the <strong>results count</em> displays the results.  From the \
+Clicking a tag adds that tag to the search; clicking the <strong>results count</strong> displays the results.  From the \
 keyboard, typing <em>Enter</em> or semicolon picks the most <strong>prominent completion</strong> or (if there's \
 no additional text) <em>Enter</em> shows the results.</p>";
 
