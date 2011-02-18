45e36c6fa6ae748fa18670615a077e9c0c3fc750
bemeta <bemeta@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2011-02-18 21:39:56 +0000

    Remove colpage default

--- sbooks.js
+++ sbooks.js
@@ -85,7 +85,7 @@ var sbook=
 	 paging: false,	// Whether to trace paging (movement by pages)
 	 scroll: false,	// Whether to trace scrolling within the HUD
 	 gestures: 0},   // Whether to trace gestures
-     colpage: true,
+     // colpage: true,
      version: codex_version, id: codex_id
     };
 var _sbook_setup=false;
