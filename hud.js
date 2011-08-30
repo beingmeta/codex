eaa3eece11010c9490ebd553b642c8b16e0c1fbe
Ken Haase <kh@beingmeta.com>
2011-08-30 14:44:46 -0400

    Moved /flyleaf path to top level

--- hud.js
+++ hud.js
@@ -583,7 +583,7 @@ var CodexMode=
 	function initManageIFrame(){
 	    var query=document.location.search||"?";
 	    var refuri=Codex.refuri;
-	    var appuri="https://"+Codex.server+"/glosses/flyleaf"+query;
+	    var appuri="https://"+Codex.server+"/flyleaf"+query;
 	    if (query.search("REFURI=")<0)
 		appuri=appuri+"&REFURI="+encodeURIComponent(refuri);
 	    if (query.search("TOPURI=")<0)
