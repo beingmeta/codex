3f371a2737330dd5c2dc199b9f5a50f146cda5fe
Ken Haase <kh@beingmeta.com>
2012-04-11 08:24:25 -0400

    Improved content tap semantics

--- interaction.js
+++ interaction.js
@@ -256,6 +256,8 @@ var codex_interaction_version=parseInt("$Revision$".slice(10,-1));
 	else CodexMode(true);}
 
     var isEmpty=fdjtString.isEmpty;
+    var last_tap=false;
+
     function content_mouseup(evt,target){
 	if (!(target)) target=fdjtUI.T(evt);
 	// Don't capture modified events
@@ -280,14 +282,24 @@ var codex_interaction_version=parseInt("$Revision$".slice(10,-1));
 	if (Codex.Trace.gestures)
 	    fdjtLog("content_mouseup (%o) on %o passage=%o mode=%o",
 		    evt,target,passage,Codex.mode);
+	// If we're not over any content, just toggle out of the HUD.
 	if (!(passage)) {
-	    CodexMode(false); fdjtUI.cancel(evt); return;}
-	if ((Codex.mode)&&
-	    ((!(seltext))||(isEmpty(seltext)))) {
 	    CodexMode(false);
-	    fdjtUI.cancel(evt);
-	    return;}
-	// If there's a selection, store it as an excerpt.
+	    fdjtUI.cancel(evt); return;}
+	// Always go and add gloss if text is selected.  Otherwise,
+	// lower the HUD if it's up or add a gloss on an apparent
+	// double click.
+	if ((!(seltext))||(isEmpty(seltext))) {
+	    if (Codex.mode) {
+		CodexMode(false); fdjtUI.cancel(evt); return;}
+	    else if (last_tap) {
+		if (last_tap!==passage) {
+		    last_tap=passage; fdjtUI.cancel(evt); return;}
+		else {
+		    // Double tap, go ahead, fall through and add a gloss
+		    last_tap=false; last_target=false;}}
+	    else {
+		last_tap=passage; fdjtUI.cancel(evt); return;}}
 	var form=Codex.setGlossTarget(passage);
 	var form_elt=fdjtDOM.getChild(form,"form");
 	var form_class='';
