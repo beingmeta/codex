43ce065c219cb2589b1d6a49d702c1f8de551545
Ken Haase <kh@beingmeta.com>
2013-06-11 13:14:55 +0200

    Turn off startup tracing

--- core.js
+++ core.js
@@ -99,7 +99,7 @@ var Codex={
     /* This is where we store pointers into the dom */
     DOM: {},
     Trace: {
-        startup: 2,       // Whether to trace startup
+        startup: 0,       // Whether to trace startup
         config: 0,        // Whether to trace config setup/modification/etc
         mode: false,      // Whether to trace mode changes
         nav: false,       // Whether to trace book navigation
